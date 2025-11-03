223c8c118905968a49abd5a225ef390d885fed59
LAURA NWENEKA <lanweneka@upei.ca>
2025-11-02 22:03:44 -0400

    2 plots

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,25 +2,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 from data import data
-from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
-from data import X_train, X_val, y_class_val, y_class_train
-from data import y_reg_val, y_reg_train
-from sklearn.linear_model import LinearRegression
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, mean_squared_error, mean_absolute_error
-import mlflow.sklearn
-import mlflow
+from data import X_train, X_test, y_class_train, y_class_test, y_reg_train, y_reg_test
+from sklearn.metrics import confusion_matrix
+from src.train_baselines import best_reg_model
+from train_baselines import best_class_model
 
-from train_baselines import y_predic_class,y_predic_reg,best_class_model
+#Plot 1 – Target distribution plot for classification (bar plot of class counts)
 plt.figure(figsize=(10,6))
 sns.countplot(x=data["label"], order=data["label"].value_counts().index)
 plt.title("Class Distribution (Crop Frequency)")
 plt.ylabel("Count")
 plt.xlabel("Crop Type")
 plt.tight_layout()
-#plt.show()
+plt.show()
 
-
+#Plot 2 – Correlation heatmap for key numeric features.
 plt.figure(figsize=(10,6))
 correlations = data.corr(numeric_only=True)
 top_4 = correlations["yield"].drop("yield").abs().sort_values(ascending=False).head(4)
@@ -39,17 +35,31 @@ plt.yticks(rotation=0)
 plt.tight_layout()
 plt.show()
 
-
+#Plot 3 – Confusion matrix for the best current classification baseline on the test set.
 model = best_class_model
+model.fit(X_train, y_class_train)
 
-y_pred = model.predict(X_val)
+y_class_pred = model.predict(X_test)
 
-
-cm = confusion_matrix(y_class_val, y_pred)
+cm = confusion_matrix(y_class_test, y_class_pred)
 plt.figure(figsize=(8, 6))
 sns.heatmap(cm, annot=True, cmap="Blues", fmt="d")
 plt.title("Confusion Matrix – Decision Tree Classifier")
 plt.xlabel("Predicted Labels")
 plt.ylabel("True Labels")
+plt.tight_layout()
+plt.show()
+
+#Plot 4 – Residuals vs predicted for the best current regression baseline on the test set
+best_reg_model.fit(X_train, y_reg_train)
+y_reg_pred = best_reg_model.predict(X_test)
+residuals = y_reg_test - y_reg_pred
+
+plt.figure(figsize=(8,6))
+plt.scatter(y_reg_pred, residuals, alpha=0.6)
+plt.axhline(y=0, color='r', linestyle='--')
+plt.title('Residuals vs Predicted – Best Regression Baseline')
+plt.xlabel('Predicted Crop Yield (kg/ha)')
+plt.ylabel('Residuals')
 plt.tight_layout()
 plt.show()
--- src/train_baselines.py
+++ src/train_baselines.py
@@ -1,12 +1,17 @@
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
-from data import X_train, X_val, y_class_val, y_class_train
-from data import y_reg_val, y_reg_train
+from data import X_train, X_val, X_test, y_class_val, y_class_train, y_class_test
+from data import y_reg_val, y_reg_train, y_reg_test
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, mean_squared_error, mean_absolute_error
-import mlflow.sklearn
+from sklearn.metrics import accuracy_score, f1_score, mean_squared_error, mean_absolute_error
 import mlflow
+import pandas as pd
+
+
+classification_results = []
+regression_results = []
+
 model_class = {
     "Naive Bays": GaussianNB(),
     "Decision Tree": DecisionTreeClassifier(random_state=42)}
@@ -16,17 +21,31 @@ model_class = {
 for name, model in model_class.items():
     with mlflow.start_run(run_name=f"Classification - {name}"):
         model.fit(X_train, y_class_train)
+        #validation
         y_predic_class = model.predict(X_val)
         acc = accuracy_score(y_class_val, y_predic_class)
         f1 = f1_score(y_class_val, y_predic_class, average='weighted')
 
-        print(f"{name} :: Accuracy: {acc:.3f}, F1: {f1:.3f}")
+        #test set
+        y_pred_test = model.predict(X_test)
+        acc_test = accuracy_score(y_class_test, y_pred_test)
+        f1_test = f1_score(y_class_test, y_pred_test, average='weighted')
 
         # Log to MLflow
         mlflow.log_param("model_type", name)
-        mlflow.log_metric("accuracy", acc)
-        mlflow.log_metric("f1_score", f1)
+        mlflow.log_metric("val_accuracy", acc)
+        mlflow.log_metric("val_f1_score", f1)
+        mlflow.log_metric("test_accuracy", acc_test)
+        mlflow.log_metric("test_f1_score", f1_test)
         mlflow.sklearn.log_model(model, name =f"{name}_classifier")
+
+        classification_results.append({
+            "Model": name,
+            "Accuracy (Validation)": round(acc, 3),
+            "F1 (Validation)": round(f1, 3),
+            "Accuracy (Test)": round(acc_test, 3),
+            "F1 (Test)": round(f1_test, 3)
+        })
 
 #Best classifier: Naive Bayes
 
@@ -38,28 +57,46 @@ model_reg = {
 for name, model in model_reg.items():
     with mlflow.start_run(run_name=f"Regression - {name}"):
         model.fit(X_train, y_reg_train)
+
+        #validation
         y_predic_reg = model.predict(X_val)
         mae = mean_absolute_error(y_reg_val,y_predic_reg)
         rmse = np.sqrt(mean_squared_error(y_reg_val,y_predic_reg))
-        print(f"{name} :: MAE: {mae:.3f}, RMSE: {rmse:.3f}")
+
+        #test
+        y_pred_test = model.predict(X_test)
+        mae_test = mean_absolute_error(y_reg_test, y_pred_test)
+        rmse_test = np.sqrt(mean_squared_error(y_reg_test, y_pred_test))
+
+
 
         # Log to MLflow
         mlflow.log_param("model_type", name)
-        mlflow.log_metric("mae", mae)
-        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("val_mae", mae)
+        mlflow.log_metric("val_rmse", rmse)
+        mlflow.log_metric("test_mae", mae_test)
+        mlflow.log_metric("test_rmse", rmse_test)
         mlflow.sklearn.log_model(model, name=f"{name}_regressor")
 
+        regression_results.append({
+            "Model": name,
+            "MAE (Validation)": round(mae, 3),
+            "RMSE (Validation)": round(rmse, 3),
+            "MAE (Test)": round(mae_test, 3),
+            "RMSE (Test)": round(rmse_test, 3)
+        })
 
 best_class_model = GaussianNB()
+best_reg_model = DecisionTreeRegressor(random_state=42)
 
+table1 = pd.DataFrame(classification_results) #Thes tables will be in our pdf
+table2 = pd.DataFrame(regression_results)
 
+print("\nTable 1 – Classification Metrics")
+print(table1.to_string(index=False))
 
-'''# confusion matrix for visualization
-cm = confusion_matrix(y_class_val, y_predic_value)
-plt.figure(figsize=(10, 8))
-sns.heatmap(cm, annot=False, cmap="Blues")
-plt.title("Confusion Matrix - Naive Bayes (Validation Set)")
-plt.xlabel("Predicted Label")
-plt.ylabel("True Label")
-plt.show()
-'''
+print("\nTable 2 – Regression Metrics")
+print(table2.to_string(index=False))
+
+table1.to_csv("Table1_Classification_Metrics.csv", index=False)
+table2.to_csv("Table2_Regression_Metrics.csv", index=False)
